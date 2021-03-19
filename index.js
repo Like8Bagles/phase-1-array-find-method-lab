05a1c753237b797a7f8d5e1f2a9e969c61d98960
Anthony <anthonykmbanks@gmail.com>
2021-03-19 04:41:09 -0400

    commit tree c335ccbe2477c7e27070e6d664aa4a3a878a38e6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function superbowlWin(record) {
+    let winOrUndefined = record.find(record => record.result=='W')
+    if (winOrUndefined.result == 'W') {
+        return winOrUndefined.year
+    }
+}
